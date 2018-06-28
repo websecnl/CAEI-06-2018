a34e9287e8c7cb1b8b644dfd272396e03adc7517
WebSEC.nl <rebitclown105@hotmail.com>
2018-06-28 18:26:08 +0200

    Update automate.py

--- automate.py
+++ automate.py
@@ -8,7 +8,7 @@ from win32api import GetSystemMetrics
 # Supported Resolutions: 1920x1080,1280x800,1600x900,1366x768,1440x900
 
 # Please note that i only Tested this on my Display, i did not code support for same resolutions on different aspect ratio's!
-# Its up to those who fork this project with to add more powerful features and support!
+# Its up to those who fork this project to add more powerful features and better support!
 
 CHROME_STAUTS = False
 MoniH = GetSystemMetrics(0)
